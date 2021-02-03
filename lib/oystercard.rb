9d1ce048cb92deb4468b21967466ca66a00861bf
JFindley95 <jessica.findley@simplybusiness.co.uk>
2021-02-03 12:40:07 +0000

    now completed chapter 10

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,31 @@
 class Oystercard
-  attr_reader :balance, :oyster_limit
+  attr_reader :balance, :oyster_limit, :in_journey
   LIMIT = 90
+  MINIMUM_CHARGE = 1
+
   def initialize(oyster_limit = LIMIT)
     @balance = 0
     @oyster_limit = oyster_limit
+    @in_journey = false
   end
+
   def top_up(num)
-    raise "Maximum amount of #{oyster_limit} already reached" if num + @balance > LIMIT
+    raise "Maximum amount of £#{oyster_limit} already reached" if num + @balance > LIMIT
     @balance += num
+  end
+
+  def touch_in
+    raise 'You need a minimum of £1 on your card to travel' if @balance < 1
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MINIMUM_CHARGE)
+    @in_journey = false
+  end
+
+private
+  def deduct(num)
+    @balance -= num
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,7 @@
 require 'oystercard'
 
   describe Oystercard do
-    let(:oystercard) { Oystercard.new(90) }
+    let(:oystercard) { Oystercard.new }
     describe '#balance' do
       it 'starts with a balance of 0' do
         expect(oystercard.balance).to eq(0)
@@ -14,8 +14,41 @@ require 'oystercard'
       end
       it 'raises error if top_up would bring balance over maximum' do
         oystercard.top_up(oystercard.oyster_limit)
-
-        expect { oystercard.top_up(5) }.to raise_error("Maximum amount of #{oystercard.oyster_limit} already reached")
+        expect { oystercard.top_up(5) }.to raise_error("Maximum amount of £#{oystercard.oyster_limit} already reached")
       end
+    end
+    # describe '#deduct' do
+    #   it 'the balance will descrease as the user taps out' do
+    #     oystercard.top_up(20)
+    #     # oystercard.deduct(5)
+    #     # expect(oystercard.balance).to eq(15)
+    #     expect{ oystercard.deduct(5)}.to change{ oystercard.balance}.by (-5)
+    #   end
+    # end
+    describe '@in_journey' do
+      it 'returns false ' do
+        expect(oystercard.in_journey).to eq(false)
       end
+    end
+    describe '#touch_in' do
+      it 'checks that the card has a minimum balance of £1' do
+      expect{ oystercard.touch_in }.to raise_error('You need a minimum of £1 on your card to travel')
+      end
+      it 'will show the user is in transit' do
+      oystercard.top_up(Oystercard::MINIMUM_CHARGE)
+      oystercard.touch_in
+      expect(oystercard.in_journey).to eq(true)
+      end
+    end
+    describe '#touch_out' do
+      it 'will show the user is NOT in transit' do
+        oystercard.touch_out
+        expect(oystercard.in_journey).to eq(false)
+      end
+      it 'will deduct the minium charge when the user touches out' do
+        oystercard.top_up(5)
+        oystercard.touch_in
+        expect{ oystercard.touch_out }.to change{ oystercard.balance }.by(-Oystercard::MINIMUM_CHARGE)
+       end
+    end
   end
